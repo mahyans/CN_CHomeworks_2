54aa5ec850c1baf940b3d0460dc30343af73df85
Fatemeh Shahhosseini <90645162+FaSha20@users.noreply.github.com>
2023-04-20 23:10:33 +0330

    Added all mappers

--- sample.cc
+++ sample.cc
@@ -90,6 +90,7 @@ public:
     MyHeader ();
     virtual ~MyHeader ();
     void SetData (uint16_t data);
+    void SetData_str(string s_data);
     uint16_t GetData (void) const;
     static TypeId GetTypeId (void);
     virtual TypeId GetInstanceTypeId (void) const;
@@ -99,6 +100,7 @@ public:
     virtual uint32_t GetSerializedSize (void) const;
 private:
     uint16_t m_data;
+    string s_data;
 };
 
 MyHeader::MyHeader ()
@@ -128,7 +130,7 @@ MyHeader::GetInstanceTypeId (void) const
 void
 MyHeader::Print (std::ostream &os) const
 {
-    os << "data = " << m_data << endl;
+    os << "m_data = " << m_data << endl;
 }
 
 uint32_t
@@ -163,10 +165,91 @@ MyHeader::GetData (void) const
     return m_data;
 }
 
+
+class StrHeader : public Header 
+{
+public:
+    StrHeader ();
+    virtual ~StrHeader ();
+    void SetData (char data);
+    char GetData (void) const;
+    static TypeId GetTypeId (void);
+    virtual TypeId GetInstanceTypeId (void) const;
+    virtual void Print (std::ostream &os) const;
+    virtual void Serialize (Buffer::Iterator start) const;
+    virtual uint32_t Deserialize (Buffer::Iterator start);
+    virtual uint32_t GetSerializedSize (void) const;
+private:
+    char m_data;
+};
+
+StrHeader::StrHeader ()
+{
+}
+
+StrHeader::~StrHeader ()
+{
+}
+
+TypeId
+StrHeader::GetTypeId (void)
+{
+    static TypeId tid = TypeId ("ns3::StrHeader")
+        .SetParent<Header> ()
+        .AddConstructor<StrHeader> ()
+    ;
+    return tid;
+}
+
+TypeId
+StrHeader::GetInstanceTypeId (void) const
+{
+    return GetTypeId ();
+}
+
+void
+StrHeader::Print (std::ostream &os) const
+{
+    os << "char:  " << m_data << endl;
+}
+
+uint32_t
+StrHeader::GetSerializedSize (void) const
+{
+    return 2;
+}
+
+void
+StrHeader::Serialize (Buffer::Iterator start) const
+{
+    start.WriteHtonU16 (m_data);
+}
+
+uint32_t
+StrHeader::Deserialize (Buffer::Iterator start)
+{
+    m_data = start.ReadNtohU16 ();
+
+    return 2;
+}
+
+void 
+StrHeader::SetData (char data)
+{
+    m_data = data;
+}
+
+char 
+StrHeader::GetData (void) const
+{
+    return m_data;
+}
+
+
 class master : public Application
 {
 public:
-    master (uint16_t port, Ipv4InterfaceContainer& ip, Ipv4InterfaceContainer& ip2);
+    master (uint16_t port,uint16_t tcp_port, Ipv4InterfaceContainer& ip, Ipv4InterfaceContainer& ip1, Ipv4InterfaceContainer& ip2, Ipv4InterfaceContainer& ip3);
     virtual ~master ();
 private:
     virtual void StartApplication (void);
@@ -174,8 +257,11 @@ private:
     void sendToMapper(uint32_t s_data, Ptr<Socket> sock);
 
     uint16_t port;
+    uint16_t tcp_port;
     Ipv4InterfaceContainer ip;
+    Ipv4InterfaceContainer& ip1;
     Ipv4InterfaceContainer& ip2;
+    Ipv4InterfaceContainer& ip3;
     Ptr<Socket> socket;
 };
 
@@ -199,19 +285,22 @@ private:
 class mapper : public Application
 {
 public:
-    mapper (uint16_t port, Ipv4InterfaceContainer& ip, Ipv4InterfaceContainer& ip2);
+    mapper (map<int,char> dictionary, uint16_t port, uint16_t tcp_port, Ipv4InterfaceContainer& ip, Ipv4InterfaceContainer& ip2);
     virtual ~mapper ();
 
 private:
     virtual void StartApplication (void);
     void HandleRead (Ptr<Socket> socket);
-    void sendToClient(uint32_t s_data, Ptr<Socket> sock);
+    void sendToClient(char s_data, Ptr<Socket> sock);
 
+    map<int,char> dictionary;
     uint16_t port;
+    uint16_t tcp_port;
     Ptr<Socket> socket;
     Ipv4InterfaceContainer ip;
     Ipv4InterfaceContainer ip2;
 };
+
 
 
 int
@@ -224,39 +313,43 @@ main (int argc, char *argv[])
     bool tracing = false;
 
     srand(time(NULL));
+/*
+    map<int,char> map1 { { 0, 100}, { 1, 101}, { 2, 102 }, { 3, 103}, { 4, 104 }, { 5, 105}, { 6, 106 },  { 7, 107}, { 8, 108 } };
+    map<int,char> map2 { { 9, 209}, { 10, 210}, { 11, 211 }, { 12, 212}, { 13, 213 }, { 14, 214}, { 15, 215 },  { 16, 216}, { 17, 217 } };
+    map<int,char> map3 { { 18, 318},{ 19, 319},{ 20, 320}, { 21, 321 }, { 22, 322}, { 23, 323 }, { 24, 324}, { 25, 325 },  { 26, 326} };
+*/
+    map<int, char> map1;
+    map<int, char> map2;
+    map<int, char> map3;
 
-    map<int,string> map1;
-    map<int,string> map2;
-    map<int,string> map3;
+    map1[0] = 'a';
+    map1[1] = 'b';
+    map1[2] = 'c';
+    map1[3] = 'd';
+    map1[4] = 'e';
+    map1[5] = 'f';
+    map1[6] = 'g';
+    map1[7] = 'h';
+    map1[8] = 'i';
 
-    map1["0"] = "a";
-    map1["1"] = "b";
-    map1["2"] = "c";
-    map1["3"] = "d";
-    map1["4"] = "e";
-    map1["5"] = "f";
-    map1["6"] = "g";
-    map1["7"] = "h";
-    map1["8"] = "i";
-
-    map2["9"] = "j";
-    map2["10"] = "k";
-    map2["11"] = "l";
-    map2["12"] = "m";
-    map2["13"] = "n";
-    map2["14"] = "o";
-    map2["15"] = "p";
-    map2["16"] = "q";
-    map2["17"] = "r";
-
-    map3["18"] = "s";
-    map3["19"] = "t";
-    map3["20"] = "u";
-    map3["21"] = "v";
-    map3["22"] = "w";
-    map3["23"] = "x";
-    map3["24"] = "y";
-    map3["25"] = "z";
+    map2[9] =  'j';
+    map2[10] = 'k';
+    map2[11] = 'l';
+    map2[12] = 'm';
+    map2[13] = 'n';
+    map2[14] = 'o';
+    map2[15] = 'p';
+    map2[16] = 'q';
+    map2[17] = 'r';
+    
+    map3[18] = 's';
+    map3[19] = 't';
+    map3[20] = 'u';
+    map3[21] = 'v';
+    map3[22] = 'w';
+    map3[23] = 'x';
+    map3[24] = 'y';
+    map3[25] = 'z';
 
     CommandLine cmd (__FILE__);
     cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
@@ -377,21 +470,33 @@ main (int argc, char *argv[])
     Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
     uint16_t port = 1102;
+    uint16_t tcp_port = 1103;
+
 
     Ptr<client> clientApp = CreateObject<client> (port, staNodesMasterInterface, staNodeClientInterface);
     wifiStaNodeClient.Get (0)->AddApplication (clientApp);
     clientApp->SetStartTime (Seconds (0.0));
     clientApp->SetStopTime (Seconds (duration));  
 
-    Ptr<master> masterApp = CreateObject<master> (port, staNodesMasterInterface, staNodesMapper1Interface);
+    Ptr<master> masterApp = CreateObject<master> (port, tcp_port, staNodesMasterInterface, staNodesMapper1Interface, staNodesMapper2Interface, staNodesMapper3Interface);
     wifiStaNodeMaster.Get (0)->AddApplication (masterApp);
     masterApp->SetStartTime (Seconds (0.0));
     masterApp->SetStopTime (Seconds (duration));  
 
-    Ptr<mapper> mapper1App = CreateObject<mapper> (port, staNodesMapper1Interface, staNodeClientInterface);
+    Ptr<mapper> mapper1App = CreateObject<mapper> (map1, port, tcp_port, staNodesMapper1Interface, staNodeClientInterface);
     wifiStaNodeMapper1.Get (0)->AddApplication (mapper1App);
     mapper1App->SetStartTime (Seconds (0.0));
     mapper1App->SetStopTime (Seconds (duration)); 
+
+    Ptr<mapper> mapper2App = CreateObject<mapper> (map2, port, tcp_port, staNodesMapper2Interface, staNodeClientInterface);
+    wifiStaNodeMapper2.Get (0)->AddApplication (mapper2App);
+    mapper2App->SetStartTime (Seconds (0.0));
+    mapper2App->SetStopTime (Seconds (duration));
+
+    Ptr<mapper> mapper3App = CreateObject<mapper> (map3, port, tcp_port, staNodesMapper3Interface, staNodeClientInterface);
+    wifiStaNodeMapper3.Get (0)->AddApplication (mapper3App);
+    mapper3App->SetStartTime (Seconds (0.0));
+    mapper3App->SetStopTime (Seconds (duration));
 
     NS_LOG_INFO ("Run Simulation");
 
@@ -428,7 +533,7 @@ client::HandleRead(Ptr<Socket> socket)
             break;
         }
 
-        MyHeader destinationHeader;
+        StrHeader destinationHeader;
         packet->RemoveHeader (destinationHeader);
         destinationHeader.Print(std::cout);
     }
@@ -448,7 +553,7 @@ static void GenerateTraffic (Ptr<Socket> socket, uint16_t data)
     packet->Print (std::cout);
     socket->Send(packet);
 
-    Simulator::Schedule (Seconds (0.1), &GenerateTraffic, socket, rand() % 26);
+    Simulator::Schedule (Seconds (0.1), &GenerateTraffic, socket, rand() % 27);
 }
 
 void
@@ -467,10 +572,13 @@ client::StartApplication (void)
     socket->SetRecvCallback (MakeCallback (&client::HandleRead, this));
 }
 
-master::master (uint16_t port, Ipv4InterfaceContainer& ip, Ipv4InterfaceContainer& ip2)
+master::master (uint16_t port,uint16_t tcp_port, Ipv4InterfaceContainer& ip, Ipv4InterfaceContainer& ip1, Ipv4InterfaceContainer& ip2, Ipv4InterfaceContainer& ip3)
         : port (port),
+          tcp_port (tcp_port),
           ip (ip),
-          ip2 (ip2)
+          ip1 (ip1),
+          ip2 (ip2),
+          ip3 (ip3)
 {
     std::srand (time(0));
 }
@@ -478,7 +586,6 @@ master::master (uint16_t port, Ipv4InterfaceContainer& ip, Ipv4InterfaceContaine
 void 
 master::sendToMapper(uint32_t s_data, Ptr<Socket> sock)
 {
-
     Ptr<Packet> packet = new Packet();
     MyHeader m;
     m.SetData(s_data);
@@ -507,9 +614,17 @@ master::HandleRead (Ptr<Socket> socket)
 {
     Ptr<Packet> packet;
 
-    Ptr<Socket> sock = Socket::CreateSocket (GetNode (), UdpSocketFactory::GetTypeId ());
-    InetSocketAddress sockAddr (ip2.GetAddress(0), port);
-    sock->Connect (sockAddr);
+    Ptr<Socket> sock1 = Socket::CreateSocket (GetNode (), UdpSocketFactory::GetTypeId ());
+    InetSocketAddress sockAddr1 (ip1.GetAddress(0), tcp_port);
+    sock1->Connect (sockAddr1);
+
+    Ptr<Socket> sock2 = Socket::CreateSocket (GetNode (), UdpSocketFactory::GetTypeId ());
+    InetSocketAddress sockAddr2 (ip2.GetAddress(0), tcp_port);
+    sock2->Connect (sockAddr2);
+
+    Ptr<Socket> sock3 = Socket::CreateSocket (GetNode (), UdpSocketFactory::GetTypeId ());
+    InetSocketAddress sockAddr3 (ip3.GetAddress(0), tcp_port);
+    sock3->Connect (sockAddr3);
 
     while ((packet = socket->Recv ()))
     {
@@ -517,18 +632,20 @@ master::HandleRead (Ptr<Socket> socket)
         {
             break;
         }
-
         MyHeader destinationHeader;
         packet->RemoveHeader (destinationHeader);
-        destinationHeader.Print(std::cout);
+        //destinationHeader.Print(std::cout);
         int val = destinationHeader.GetData();
-        sendToMapper( val, sock);
-
+        sendToMapper( val, sock1);
+        sendToMapper( val, sock2);
+        sendToMapper( val, sock3);
     }
 }
 
-mapper::mapper (uint16_t port, Ipv4InterfaceContainer& ip, Ipv4InterfaceContainer& ip2)
-        : port (port),
+mapper::mapper (map<int,char> m, uint16_t port, uint16_t tcp_port, Ipv4InterfaceContainer& ip, Ipv4InterfaceContainer& ip2)
+        : dictionary (m), 
+          port (port),
+          tcp_port (tcp_port),
           ip (ip),
           ip2 (ip2)
 {
@@ -543,7 +660,7 @@ void
 mapper::StartApplication (void)
 {
     socket = Socket::CreateSocket (GetNode (), UdpSocketFactory::GetTypeId ());
-    InetSocketAddress local = InetSocketAddress (ip.GetAddress(0), port);
+    InetSocketAddress local = InetSocketAddress (ip.GetAddress(0), tcp_port);
     socket->Bind (local);
 
     socket->SetRecvCallback (MakeCallback (&mapper::HandleRead, this));
@@ -567,19 +684,21 @@ mapper::HandleRead (Ptr<Socket> socket)
 
         MyHeader destinationHeader;
         packet->RemoveHeader (destinationHeader);
-        destinationHeader.Print(std::cout);
-        int val = destinationHeader.GetData();
-        sendToClient( 3*val, sock);
-
+        
+        uint16_t val = destinationHeader.GetData();
+        
+        if (dictionary.find(val) != dictionary.end()){
+            sendToClient( dictionary[val], sock);
+        }
     }
 }
 
-void 
-mapper::sendToClient(uint32_t s_data, Ptr<Socket> sock)
+void
+mapper::sendToClient(char s_data, Ptr<Socket> sock)
 {
 
     Ptr<Packet> packet = new Packet();
-    MyHeader m;
+    StrHeader m;
     m.SetData(s_data);
 
     packet->AddHeader (m);
